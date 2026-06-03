178fc674965994d7d9f9df5688227699443f80fb
Dan <Daphnis1979@gmail.com>
2026-06-03 16:43:30 -0400

    draft insert queries need work

--- insert_data.sh
+++ insert_data.sh
@@ -8,7 +8,7 @@ else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 
 fi
-
+$($PSQL "TRUNCATE teams games")
 # Do not change code above this line. Use the PSQL variable above to query your database.
 cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
@@ -30,9 +30,14 @@ do
   then
     #INSERT POPPONENT TEAM
     $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
+    #OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
   fi
-  #GET NEW TEAM_ID
 
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR'OUND''$WINNER_ID''$OPPONENT_ID''$WINNER_GOALS''$OPPONENT_GOALS')")
+ 
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
+  $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES('$YEAR''$ROUND''$WINNER_ID''$OPPONENT_ID''$WINNER_GOALS''$OPPONENT_GOALS')")
+ 
  fi
  done
