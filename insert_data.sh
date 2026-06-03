e4f073698c707019795483b7029be9086271ec83
Dan <Daphnis1979@gmail.com>
2026-06-03 16:09:40 -0400

    first pass at table construction

--- insert_data.sh
+++ insert_data.sh
@@ -3,8 +3,36 @@
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]]
+  then
+    #get winner team Id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  #IF NOT FOUND
+  if [[ -z $WINNER_ID ]]
+  then
+    #INSERT WINNER TEAM
+    $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+  fi
+  #get opponent team_id
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
+  #if not found
+    if [[ -z $OPPONENT_ID ]]
+  then
+    #INSERT POPPONENT TEAM
+    $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
+  fi
+  #GET NEW TEAM_ID
+
+ fi
+ done
